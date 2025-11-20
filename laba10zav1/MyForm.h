6e28f63d7c2b65520d6cb14f2c777179784939e5
Leragolovata <lera1golovata@gmail.com>
2025-11-20 18:52:50 +0200

    Add files via upload

---
+++ laba10zav1/MyForm.h
@@ -0,0 +1,145 @@
+#pragma once
+
+namespace laba10zav1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(40, 126);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(167, 16);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"¬вед≥ть 10 д≥йсних чисел";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(228, 126);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(455, 67);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(259, 226);
+			this->textBox2->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(148, 291);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(121, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"ќбчислити";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(336, 291);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(113, 23);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"ќчистити";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(778, 447);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		double x;
+		int count = 0;
+
+		// „итаЇмо вс≥ числа через проб≥л
+		System::String^ s = textBox1->Text;
+		array<System::String^>^ a = s->Split(' ');
+
+		for each (System::String ^ t in a) {
+			if (t != "" && Double::TryParse(t, x) && x > 0)
+				count++;
+		}
+
+		textBox2->Text = count.ToString();
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	textBox1->Clear();
+	textBox2->Clear();
+}
+};
+}
